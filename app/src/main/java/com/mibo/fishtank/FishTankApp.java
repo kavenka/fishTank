d69799df80cbdb0477ba1ff9ec84363e614d9c71
kavenka <kavenka@sina.com>
2017-06-05 00:03:18 +0800

    添加推送SDK

--- app/src/main/java/com/mibo/fishtank/FishTankApp.java
+++ app/src/main/java/com/mibo/fishtank/FishTankApp.java
@@ -1,6 +1,8 @@
 package com.mibo.fishtank;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
 import com.mibo.fishtank.FishTankmManage.FishTankApiManager;
@@ -10,6 +12,8 @@ import com.tencent.android.tpush.XGPushConfig;
 import com.tencent.android.tpush.XGPushManager;
 
 import org.litepal.LitePal;
+
+import java.util.List;
 
 /**
  * Created by Administrator
@@ -27,22 +31,36 @@ public class FishTankApp extends Application {
     public void onCreate() {
         super.onCreate();
         mFishTankApp = this;
-        // 开启logcat输出，方便debug，发布时请关闭
-        XGPushConfig.enableDebug(this, true);
-
-        XGPushManager.registerPush(this, new XGIOperateCallback() {
-            @Override
-            public void onSuccess(Object data, int flag) {
-                Log.d("TPush", "注册成功，设备token为：" + data);
-            }
-
-            @Override
-            public void onFail(Object data, int errCode, String msg) {
-                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
-            }
-        });
         LitePal.initialize(this);
         FishTankApiManager.getInstance().init();
         FishTankUserApiManager.getInstance().init();
+        // 开启logcat输出，方便debug，发布时请关闭
+        XGPushConfig.enableDebug(this, true);
+        if (isMainProcess()) {
+            XGPushManager.registerPush(this, new XGIOperateCallback() {
+                @Override
+                public void onSuccess(Object data, int flag) {
+                    Log.d("TPush", "注册成功，设备token为：" + data);
+                }
+
+                @Override
+                public void onFail(Object data, int errCode, String msg) {
+                    Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+                }
+            });
+        }
+    }
+
+    public boolean isMainProcess() {
+        ActivityManager am = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE));
+        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+        String mainProcessName = getPackageName();
+        int myPid = android.os.Process.myPid();
+        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
+            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
