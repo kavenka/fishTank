b49af1de45bee9361b0e9f666bb45ea3082e8e46
kavenka <kavenka@sina.com>
2017-06-04 19:10:33 +0800

    添加推送SDK

--- app/src/main/java/com/mibo/fishtank/FishTankApp.java
+++ app/src/main/java/com/mibo/fishtank/FishTankApp.java
@@ -1,9 +1,13 @@
 package com.mibo.fishtank;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.mibo.fishtank.FishTankmManage.FishTankApiManager;
 import com.mibo.fishtank.FishTankmManage.FishTankUserApiManager;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
 
 import org.litepal.LitePal;
 
@@ -23,6 +27,20 @@ public class FishTankApp extends Application {
     public void onCreate() {
         super.onCreate();
         mFishTankApp = this;
+        // 开启logcat输出，方便debug，发布时请关闭
+        XGPushConfig.enableDebug(this, true);
+
+        XGPushManager.registerPush(this, new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int flag) {
+                Log.d("TPush", "注册成功，设备token为：" + data);
+            }
+
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });
         LitePal.initialize(this);
         FishTankApiManager.getInstance().init();
         FishTankUserApiManager.getInstance().init();
--- app/src/main/java/com/mibo/fishtank/activity/LaunchActivity.java
+++ app/src/main/java/com/mibo/fishtank/activity/LaunchActivity.java
@@ -1,6 +1,5 @@
 package com.mibo.fishtank.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +8,6 @@ import android.widget.Button;
 import com.mibo.fishtank.R;
 import com.mibo.fishtank.utils.PreferencesManager;
 import com.tencent.android.tpush.XGPushConfig;
-import com.tencent.android.tpush.XGPushManager;
 
 public class LaunchActivity extends BaseActivity {
 
@@ -20,14 +18,7 @@ public class LaunchActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 开启logcat输出，方便debug，发布时请关闭
         XGPushConfig.enableDebug(this, true);
-        // 如果需要知道注册是否成功，请使用registerPush(getApplicationContext(), XGIOperateCallback)带callback版本
-// 如果需要绑定账号，请使用registerPush(getApplicationContext(),account)版本
-// 具体可参考详细的开发指南
-// 传递的参数为ApplicationContext
-        Context context = getApplicationContext();
-        XGPushManager.registerPush(context);
         setContentView(R.layout.launch_activity);
         initView();
         getLoginInfo();
