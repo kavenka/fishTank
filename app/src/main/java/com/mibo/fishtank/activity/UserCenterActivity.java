23d119409083e644255392bd165da8d8608e5d75
Monty <wm_8800@163.com>
2017-06-19 22:26:39 +0800

    修改个人信息页面增加权限校验

--- app/src/main/java/com/mibo/fishtank/activity/UserCenterActivity.java
+++ app/src/main/java/com/mibo/fishtank/activity/UserCenterActivity.java
@@ -1,8 +1,10 @@
 package com.mibo.fishtank.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,6 +14,12 @@ import com.mibo.fishtank.R;
 import com.mibo.fishtank.utils.GlideUtils;
 import com.mibo.fishtank.utils.PreferencesManager;
 import com.mibo.fishtank.weight.TitleBar;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.PermissionListener;
+import com.yanzhenjie.permission.Rationale;
+import com.yanzhenjie.permission.RationaleListener;
+
+import java.util.List;
 
 public class UserCenterActivity extends BaseActivity {
     private Context context = this;
@@ -30,7 +38,7 @@ public class UserCenterActivity extends BaseActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        GlideUtils.showUserIcon(this,imageView);
+        GlideUtils.showUserIcon(this, imageView);
     }
 
     private void initView() {
@@ -49,8 +57,8 @@ public class UserCenterActivity extends BaseActivity {
         PreferencesManager pm = PreferencesManager.getInstance(context);
         String nickNameStr = pm.getStringValue("nikeName");
         String telStr = pm.getStringValue("tel");
-        this.nickName.setText(TextUtils.isEmpty(nickNameStr) ? "尚未设置": nickNameStr);
-        this.tel.setText(TextUtils.isEmpty(telStr)?"尚未设置":telStr);
+        this.nickName.setText(TextUtils.isEmpty(nickNameStr) ? "尚未设置" : nickNameStr);
+        this.tel.setText(TextUtils.isEmpty(telStr) ? "尚未设置" : telStr);
 
     }
 
@@ -79,8 +87,35 @@ public class UserCenterActivity extends BaseActivity {
     private class OnClickRightListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(context, EditUserInfoActivity.class);
-            startActivityForResult(intent, TO_EDIT_INFO_CODE);
+
+            AndPermission.with(UserCenterActivity.this)
+                    .requestCode(100)
+                    .permission(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .rationale(new RationaleListener() {
+                        @Override
+                        public void showRequestPermissionRationale(int requestCode, Rationale rationale) {
+                            AndPermission.rationaleDialog(context, rationale).show();
+                        }
+                    })
+                    .callback(new PermissionListener() {
+                        @Override
+                        public void onSucceed(int requestCode, @NonNull List<String> grantPermissions) {
+                            Intent intent = new Intent(context, EditUserInfoActivity.class);
+                            startActivityForResult(intent, TO_EDIT_INFO_CODE);
+                        }
+
+                        @Override
+                        public void onFailed(int requestCode, @NonNull List<String> deniedPermissions) {
+//                    Toast.makeText(UserCenterActivity.this,"请先授权，否则无法修改个人信息",Toast.LENGTH_SHORT).show();
+                            AndPermission.defaultSettingDialog(UserCenterActivity.this, 400)
+                                    .setTitle("权限申请失败")
+                                    .setMessage("您拒绝了我们必要的一些权限，已经没法愉快的玩耍了，请在设置中授权！")
+                                    .setPositiveButton("好，去设置")
+                                    .show();
+                        }
+                    }).start();
         }
     }
+
+
 }
